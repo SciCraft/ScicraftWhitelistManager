d981f7e0f1a7e5a87e0d939e93922a09978bbc9c
FX <fx.e.morin@gmail.com>
2021-09-27 15:52:55 -0400

    Changing logging behavior

--- Logger.js
+++ Logger.js
@@ -13,32 +13,29 @@ const logger = createLogger({
     ]
 });
 
-configure({ //Must be done through configure so the default loggers use them
-    transports: [
-        new transports.File({
-            format: format.combine(
-                format.timestamp({
-                    format: 'DD-MM-YYYY HH:mm:ss'
-                }),
-                format.simple()
-            ),
-            filename: 'logs/combined.log',
-            level: 'debug'
+logger.add(new transports.File({
+    format: format.combine(
+        format.timestamp({
+            format: 'DD-MM-YYYY HH:mm:ss'
         }),
-        new transports.File({
-            format: format.combine(
-                format.timestamp({
-                    format: 'DD-MM-YYYY HH:mm:ss'
-                }),
-                format.errors({ stack: true }),
-                format.simple()
-            ),
-            handleExceptions: true,
-            filename: 'logs/errors.log',
-            level: 'error'
-        })
-    ]
-});
+        format.simple()
+    ),
+    filename: 'logs/combined.log',
+    level: 'debug'
+}));
+
+logger.add(new transports.File({
+    format: format.combine(
+        format.timestamp({
+            format: 'DD-MM-YYYY HH:mm:ss'
+        }),
+        format.errors({ stack: true }),
+        format.simple()
+    ),
+    handleExceptions: true,
+    filename: 'logs/errors.log',
+    level: 'error'
+}));
 
 logger.on('finish', function () {
     console.info('Logger has finished logging!');
